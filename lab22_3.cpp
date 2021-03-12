503e0302200e1412db5244ff599eca309c11c84d
Som3on31 <saharit.f4@gmail.com>
2021-03-12 15:55:00 +0700

    Deleted dupe codes.

--- lab22_3.cpp
+++ lab22_3.cpp
@@ -1,8 +1,19 @@
 #include<iostream>
+#include<set>
 
 using namespace std;
 
-//Write function count() here.
+int count(int data[1000],int size){
+	set <int> uniqueData;
+
+	for (unsigned int i=0;i<size;i++){
+		int temp = data[i];
+		
+		
+	}
+
+	return uniqueData.size();
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
